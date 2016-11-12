ad8cae099490c1f3216eb790d9560003a610e0fd
Pavel Perestoronin <pavel.perestoronin@booking.com>
2016-11-12 20:58:16 +0100

    Translate comments due to buggy checker

--- MyStrategy.py
+++ MyStrategy.py
@@ -56,10 +56,10 @@ class MyStrategy:
         SkillType.SHIELD,
     ]
 
-    # Пути перемещения по карте.
+    # Ways to move around the map.
     WAY_POINTS = {
         LaneType.TOP: [
-            # Идем наверх.
+            # Move upwards.
             (200.0, 3400.0),
             (200.0, 3000.0),
             (200.0, 2600.0),
@@ -69,7 +69,7 @@ class MyStrategy:
             (200.0, 1000.0),
             (200.0, 600.0),
             (200.0, 200.0),
-            # Идем вправо.
+            # Move right.
             (600.0, 200.0),
             (1000.0, 200.0),
             (1400.0, 200.0),
@@ -81,10 +81,10 @@ class MyStrategy:
             (3800.0, 200.0),
         ],
         LaneType.MIDDLE: [
-            # Обойти базу.
+            # Move around the base.
             (200.0, 3400.0),
             (600.0, 3400.0),
-            # Бредем по диагонали.
+            # Move along diagonal.
             (1000.0, 3000.0),
             (1400.0, 2600.0),
             (1800.0, 2200.0),
@@ -92,12 +92,12 @@ class MyStrategy:
             (2600.0, 1400.0),
             (3000.0, 1000.0),
             (3400.0, 600.0),
-            # Обходим базу.
+            # Move around the base.
             (3400.0, 200.0),
             (3800.0, 200.0),
         ],
         LaneType.BOTTOM: [
-            # Идем вправо.
+            # Move right.
             (600.0, 3800.0),
             (1000.0, 3800.0),
             (1400.0, 3800.0),
@@ -107,7 +107,7 @@ class MyStrategy:
             (3000.0, 3800.0),
             (3400.0, 3800.0),
             (3800.0, 3800.0),
-            # Идем наверх.
+            # Move upwards.
             (3800.0, 3400.0),
             (3800.0, 3000.0),
             (3800.0, 2600.0),
@@ -121,27 +121,27 @@ class MyStrategy:
     }
 
     def __init__(self):
-        # По умолчанию выбираем случайное направление.
+        # Choose random lane by default.
         self.lane_type = random.choice([LaneType.TOP, LaneType.MIDDLE, LaneType.BOTTOM])
         self.way_point_index = 0
         self.reverse = False
 
     # noinspection PyMethodMayBeStatic
     def move(self, me: Wizard, world: World, game: Game, move: Move):
-        # Сначала всякие общие вещи.
+        # First, common things.
         my_player = world.get_my_player()  # type: Player
         opponent_faction = self.opponent_faction_to(my_player.faction)
         skills = set(me.skills)
 
-        # Изучаем следующий навык.
+        # Learn some skill.
         move.skill_to_learn = self.skill_to_learn(skills)
 
-        # Спасаем свою жизнь.
+        # Save my own life.
         if me.life < 0.5 * me.max_life:
-            # У нас осталось мало жизни. Отступаем.
+            # We're unhealthy. Retreat.
             self.move_to_next_way_point(me, game, move, reverse=True)
             return
-        # Суммируем жизнь союзников и врагов в зоне обстрела.
+        # Sum up allies and enemies lives.
         allies_life = sum(map(operator.attrgetter("life"), itertools.chain(
             self.filter_units_by_distance(me, self.filter_units_by_faction(world.minions, me.faction), me.cast_range),
             self.filter_units_by_distance(me, self.filter_units_by_faction(world.wizards, me.faction), me.cast_range),
@@ -151,54 +151,54 @@ class MyStrategy:
             self.filter_units_by_distance(me, self.filter_units_by_faction(world.wizards, opponent_faction), me.cast_range),
         )))
         if enemies_life > 1.5 * allies_life:
-            # Враги сильнее, спасаемся бегством.
+            # Enemies life is much greater. Retreat.
             self.move_to_next_way_point(me, game, move, reverse=True)
             return
-        # Обнаруживаем цели.
+        # Discover targets.
         targets = list(itertools.chain(
             self.filter_units_by_faction(world.minions, opponent_faction),
             self.filter_units_by_faction(world.wizards, opponent_faction),
             self.filter_units_by_faction(world.buildings, opponent_faction),
         ))
         if targets:
-            # Просто ищем ближайшую цель.
+            # Just look for the nearest target.
             target = min(targets, key=(lambda target_: me.get_distance_to_unit(target_)))
             action_type, min_cast_distance = self.check_attack_distance(me, game, skills, target)
             is_oriented, cast_angle = self.is_oriented_to_unit(me, game, target)
             if action_type != ActionType.NONE:
-                # Можем что-то кастануть.
+                # We can cast something.
                 if is_oriented:
-                    # Атаковать.
+                    # Attack!
                     move.cast_angle = cast_angle
                     move.action = action_type
                     move.min_cast_distance = min_cast_distance
                     return
-                # Поворачиваемся на врага.
+                # Turn around to the enemy.
                 move.turn = me.get_angle_to_unit(target)
                 return
-        # Просто двигаемся вперед.
+        # Just move straight.
         self.move_to_next_way_point(me, game, move)
 
     @staticmethod
     def opponent_faction_to(faction: Faction):
         """
-        Возвращает фракцию противника.
+        Get enemy faction.
         """
         return Faction.ACADEMY if faction == Faction.RENEGADES else Faction.RENEGADES
 
     @staticmethod
     def is_oriented_to_unit(me: Wizard, game: Game, unit: CircularUnit) -> (bool, float):
         """
-        Проверяет, наведен ли маг на цель.
+        Check if the wizard is oriented to the unit.
         """
         angle_to_unit = me.get_angle_to_unit(unit)
         cast_angle = abs(angle_to_unit) - math.atan(unit.radius / me.get_distance_to_unit(unit))
         if cast_angle < 0.0:
-            # Уже смотрим в сторону противника. Можно стрелять прямо перед собой.
+            # We can attack.
             return True, 0.0
-        # Пробуем стрелять под углом.
+        # Attack with some cast angle.
         if cast_angle < game.staff_sector / 2.0:
-            # Кастуем в ту же сторону, что и угол до цели.
+            # Cast angle has the same sign as turn angle.
             return True, math.copysign(cast_angle, angle_to_unit)
         # :(
         return False, None
@@ -206,72 +206,59 @@ class MyStrategy:
     @staticmethod
     def check_attack_distance(me: Wizard, game: Game, skills: Set[SkillType], unit: CircularUnit) -> (ActionType, float):
         """
-        Проверяет расстояние до цели и возвращает тип действия для атаки.
+        Checks distance to the unit and returns action type if possible.
         """
         distance_to_unit = me.get_distance_to_unit(unit)
         min_cast_distance = distance_to_unit - unit.radius
         if distance_to_unit < game.staff_range:
-            # Бьем посохом в ближнем бою.
             return ActionType.STAFF, min_cast_distance
         if distance_to_unit > me.cast_range:
-            # Слишком далеко.
             return ActionType.NONE, min_cast_distance
-        # Пытаемся кастануть магию.
         if SkillType.FIREBALL in skills and me.mana > game.fireball_manacost:
             return ActionType.FIREBALL, min_cast_distance
         if SkillType.FROST_BOLT in skills and me.mana > game.frost_bolt_manacost:
             return ActionType.FROST_BOLT, min_cast_distance
         if me.mana > game.magic_missile_manacost:
             return ActionType.MAGIC_MISSILE, min_cast_distance
-        # Ничего не выходит.
         return ActionType.NONE, min_cast_distance
 
     @staticmethod
     def skill_to_learn(skills: Set[SkillType]) -> SkillType:
         """
-        Возвращает следующий навык для изучения.
+        Get the next skill to learn.
         """
         for skill in MyStrategy.SKILL_ORDER:
-            # Просто ищем первый не изученный навык в порядке приоритета.
+            # Just look for the first skill in the list.
             if skill not in skills:
                 return skill
 
     @staticmethod
     def filter_units_by_faction(units: Iterable[Unit], faction: Faction):
-        """
-        Фильтрует юниты по фракции.
-        """
         return (unit for unit in units if unit.faction == faction)
 
     @staticmethod
     def filter_units_by_distance(me: Wizard, units: Iterable[Unit], distance: float):
-        """
-        Фильтрует юниты по расстроянию.
-        """
         return (unit for unit in units if me.get_distance_to_unit(unit) < distance)
 
     def move_to_next_way_point(self, me: Wizard, game: Game, move: Move, reverse=False):
-        """
-        Движение к следующей ключевой точке.
-        """
         way_points = MyStrategy.WAY_POINTS[self.lane_type]
         if me.x < 400.0 and me.y > 3600.0:
-            # Появились возле базы.
+            # We appeared near the base.
             self.way_point_index = 0
         if self.reverse != reverse:
-            # Изменили текущее направление движения. Скорректировать точку.
+            # Moving direction has been changed. Fix the way point.
             self.way_point_index += +1 if not reverse else -1
             self.reverse = reverse
         if self.way_point_index <= -1 or self.way_point_index >= len(way_points):
-            # Достигли крайней точки. Стоим на месте.
+            # Reached the final destination. Stop.
             return
-        # Двигаемся в сторону точки назначения.
+        # Move towards the way point.
         x, y = way_points[self.way_point_index]
         move.turn = me.get_angle_to(x, y)
         if abs(move.turn) < game.staff_sector / 2.0:
-            # Повернулись на точку, можно идти.
+            # Turn is finished. We can move.
             move.speed = game.wizard_forward_speed
-        # Проверяем достижение точки назначения.
+        # Check if we reached the destination.
         if me.get_distance_to(x, y) < me.radius:
-            # Достигли точки назначения, берем следующую или предыдущую.
+            # We reached the destination. Switch the next way point.
             self.way_point_index = self.way_point_index + 1 if not reverse else self.way_point_index - 1
